10022857f79a83f178d64206e9318f1dbf7a44de
Wei Li <weili.mail@gmail.com>
2025-11-23 06:34:37 -0500

    Add README.adoc

--- src/main/java/com/example/demo/config/HttpClientConfig.java
+++ src/main/java/com/example/demo/config/HttpClientConfig.java
@@ -1,7 +1,5 @@
 package com.example.demo.config;
 
-import com.example.demo.client.jph.JsonPlaceholderClient;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -14,11 +12,12 @@ import org.springframework.security.oauth2.client.web.client.support.OAuth2RestC
 import org.springframework.web.client.support.RestClientHttpServiceGroupConfigurer;
 import org.springframework.web.service.registry.ImportHttpServices;
 
+import com.example.demo.client.jph.JsonPlaceholderClient;
+
 @Configuration
 @ImportHttpServices(
         group = "jph",
         types = {JsonPlaceholderClient.class})
-// @ImportHttpServices( group = "ara", types = {RestfulApiClient.class})
 @ImportHttpServices(group = "github", basePackages = "com.example.demo.client.github")
 @Import(MyHttpServiceRegistrar.class)
 public class HttpClientConfig {
